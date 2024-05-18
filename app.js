1eaaf21f565b92428d774e8405bed43709f12a90
mamawski01 <caretib123@gmail.com>
2024-05-18 09:56:10 +0800

    ok
    ok

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-setInterval(() => {
-  console.log('Hello World');
-}, 1000);
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Hello Earth!');
+});
+
+const port = process.env.PORT || 1000;
+
+const server = http.createServer(app);
+
+app.use(express.static('public')); // serve static files from public folder;
+
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+const io = require('socket.io')(server);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
